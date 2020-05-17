811c5a03d54745bc75d98300306041edbbf9cb84
Ihor Hnat <ihor.hnat@gmail.com>
2020-05-17 21:44:59 +0300

    added linewidthdialog

---
+++ Doodlze20/app/src/main/java/com/example/doodlze20/LineWidthDialogFragment.java
@@ -0,0 +1,108 @@
+package com.example.davit.doodlz;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class LineWidthDialogFragment extends DialogFragment {
+
+    private ImageView widthImageView;
+    private MainActivityFragment doodleFragment;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        MainActivityFragment fragment = getDoodleFragment();
+
+        if(fragment != null) {
+            fragment.setDialogOnScreen(true);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        MainActivityFragment fragment = getDoodleFragment();
+
+        if(fragment != null) {
+            fragment.setDialogOnScreen(false);
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        View widthDialogView = getActivity().getLayoutInflater().inflate(
+                R.layout.fragment_line_width,
+                null
+        );
+        builder.setView(widthDialogView);
+        builder.setTitle(R.string.title_line_width_dialog);
+        widthImageView = (ImageView) widthDialogView.findViewById(R.id.widthImageView);
+
+        final DoodleView doodleView = getDoodleFragment().getDoodleView();
+        final SeekBar widthSeekBar = (SeekBar) widthDialogView.findViewById(R.id.widthSeekBar);
+        widthSeekBar.setOnSeekBarChangeListener(lineWidthChanged);
+        widthSeekBar.setProgress(doodleView.getLineWidth());
+        builder.setPositiveButton(R.string.button_set_line_width,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        doodleView.setLineWidth(widthSeekBar.getProgress());
+                    }
+                });
+        return builder.create();
+    }
+
+    public MainActivityFragment getDoodleFragment() {
+        return (MainActivityFragment) getFragmentManager()
+                .findFragmentById(R.id.doodleFragment);
+    }
+
+    // TODO : Додати мову для заміни форми пензлика
+    private SeekBar.OnSeekBarChangeListener lineWidthChanged = new SeekBar.OnSeekBarChangeListener() {
+
+        final Bitmap bitmap = Bitmap.createBitmap(400, 100, Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(bitmap);
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            Paint p = new Paint();
+            p.setColor(getDoodleFragment().getDoodleView().getDrawingColor());
+            p.setStrokeCap(Paint.Cap.SQUARE);
+            p.setStrokeWidth(progress);
+
+            // Стрирання bitmap і зміна ліній
+            bitmap.eraseColor(
+                    ContextCompat.getColor(getActivity(), android.R.color.transparent));
+            canvas.drawLine(30, 50, 370, 50, p);
+            widthImageView.setImageBitmap(bitmap);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+}
